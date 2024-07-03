04d4eacdc80d447e0ef57cc68b3d83b180cb0608
Jianming Tong <jianming.gatech@gmail.com>
2024-07-03 23:28:40 +0000

    update partitioning no hp

---
+++ multiface_partition_bdct4x4_nohp/launch_train_from_scratch_job_serial_uwing.py
@@ -0,0 +1,27 @@
+import os
+
+# Please select one path out of the following three paths based on the server
+path_prefix_uwing2 = "/workspace/uwing2/multiface/"
+
+average_texture_path = "dataset/m--20180227--0000--6795937--GHS/unwrapped_uv_1024/E001_Neutral_Eyes_Open/average/000102.png"
+# Please select one path out of the following three paths based on the server
+
+#######
+path_prefix = path_prefix_uwing2
+path_variance_matrix_tensor = "/workspace/uwing2/Privatar/testing_results/dummy_path"
+prefix_path_captured_latent_code = ""
+#######
+
+average_texture_path = path_prefix + average_texture_path
+BDCT_threshold = [1.2]# [0.1, 0.3, 0.305, 0.35, 0.4, 0.42, 0.45, 0.5, 0.6, 0.63, 0.7, 1.035, 1.1, 1.2, 3.5, 5]
+
+# On syenrgy3 machine --- the following codes should be executed
+# scl enable devtoolset-11 bash
+#  --master_port=25678
+for BDCT_thres in BDCT_threshold:
+    result_path = f"/workspace/uwing2/Privatar/training_results/bdct_4x4_nohp_{str(BDCT_thres)}"
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+    
+    print(f'python3 train_uwing2.py --data_dir {path_prefix_uwing2}dataset/m--20180227--0000--6795937--GHS --krt_dir {path_prefix_uwing2}dataset/m--20180227--0000--6795937--GHS/KRT --framelist_train {path_prefix_uwing2}dataset/m--20180227--0000--6795937--GHS/frame_list.txt --framelist_test {path_prefix_uwing2}dataset/m--20180227--0000--6795937--GHS/frame_list.txt --average_texture_path {average_texture_path} --result_path {result_path} --test_segment "./test_segment.json" --lambda_screen 1  --arch base --frequency_threshold {BDCT_thres} --project_name bdct4x4_hp_training --author_name jimmytong --path_variance_matrix_tensor {path_variance_matrix_tensor} --prefix_path_captured_latent_code {path_variance_matrix_tensor}')
+    os.system(f'python3 train_uwing2.py --data_dir {path_prefix_uwing2}dataset/m--20180227--0000--6795937--GHS --krt_dir {path_prefix_uwing2}dataset/m--20180227--0000--6795937--GHS/KRT --framelist_train {path_prefix_uwing2}dataset/m--20180227--0000--6795937--GHS/frame_list.txt --framelist_test {path_prefix_uwing2}dataset/m--20180227--0000--6795937--GHS/frame_list.txt --average_texture_path {average_texture_path} --result_path {result_path} --test_segment "./test_segment.json" --lambda_screen 1  --arch base --frequency_threshold {BDCT_thres} --project_name bdct4x4_hp_training --author_name jimmytong --path_variance_matrix_tensor {path_variance_matrix_tensor} --prefix_path_captured_latent_code {path_variance_matrix_tensor}')
---
+++ multiface_partition_bdct4x4_nohp/script_JT.sh
@@ -0,0 +1,20 @@
+# Training script
+torchrun --nproc_per_node=1 train.py --data_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS --krt_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_train /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/frame_list.txt --framelist_test /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path "/home/jianming/work/Privatar_prj/training_results" --test_segment "./test_segment.json" --lambda_screen 1 --model_ckpt "/home/jianming/work/multiface/pretrained_model/6795937_best_base_model.pth" --arch base
+
+
+# Testing script #test samples 44536
+torchrun --nproc_per_node=1 test.py --data_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS --krt_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_test /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path "/home/jianming/work/Privatar_prj/testing_results" --test_segment "./test_segment.json" --lambda_screen 1 --model_path "/home/jianming/work/multiface/pretrained_model/6795937_best_base_model.pth" --arch base
+
+
+# Visualization script #test samples 44536 -- could use for testing
+torchrun --nproc_per_node=1 visualize.py --data_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS --krt_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_test /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path "/home/jianming/work/Privatar_prj/visual_results" --test_segment_config "./test_segment.json" --lambda_screen 1 --model_path "/home/jianming/work/multiface/pretrained_model/6795937_best_base_model.pth" --arch base --camera_config /home/jianming/work/Privatar_prj/multiface/camera_configs/camera-split-config_6795937.json --camera_setting "full"
+
+
+# Testing script -- partitioned model #test samples 44536
+torchrun --nproc_per_node=1 --master_port=25678  test.py --data_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS --krt_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_test /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path "/home/jianming/work/Privatar_prj/testing_results/horizontal_partition_4.0" --test_segment "./test_segment.json" --lambda_screen 1 --model_path "/home/jianming/work/Privatar_prj/training_results/horizontal_partition_0.4/best_model.pth" --frequency_threshold 0.4 --arch base
+
+
+# Testing script -- partitioned model #test samples 44536 -- uwing2
+torchrun --nproc_per_node=1 --master_port=25680  test_uwing2.py --data_dir /workspace/uwing2/multiface/dataset/m--20180227--0000--6795937--GHS --krt_dir /workspace/uwing2/multiface/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_test /workspace/uwing2/multiface/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path /workspace/uwing2/Privatar/partition_results/horizontal_partition_0.4 --test_segment "./test_segment.json" --lambda_screen 1 --model_path "/workspace/uwing2/Privatar/partition_results/horizontal_partition_0.4/best_model.pth" --average_texture_path "/workspace/uwing2/multiface/dataset/m--20180227--0000--6795937--GHS/unwrapped_uv_1024/E001_Neutral_Eyes_Open/average/000102.png" --frequency_threshold 0.4 --arch base
+torchrun --nproc_per_node=1 --master_port=25678  test.py --data_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS --krt_dir /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_test /home/jianming/work/multiface/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path "/home/jianming/work/Privatar_prj/testing_results/horizontal_partition_0.4" --test_segment "./test_segment.json" --lambda_screen 1 --model_path "/home/jianming/work/Privatar_prj/training_results/horizontal_partition_0.4/best_model.pth" --frequency_threshold 0.4 --arch base
+
