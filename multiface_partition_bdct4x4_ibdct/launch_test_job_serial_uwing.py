7b55d9d371a6ad188562ee69c91454010e383083
Jianming Tong <jianming.gatech@gmail.com>
2024-08-31 22:06:09 -0400

    update epochs as a new parameters

--- multiface_partition_bdct4x4_ibdct/launch_test_job_serial_uwing.py
+++ multiface_partition_bdct4x4_ibdct/launch_test_job_serial_uwing.py
@@ -4,13 +4,13 @@ import os
 data_dir = "/workspace/uwing2/multiface"
 result_path_prefix = "/workspace/uwing2/Privatar/testing_results/"
 
-val_batch_size = 22
-num_freq_comp_outsourced = 0
+val_batch_size = 24
+num_freq_comp_outsourced = 2
 project_name = f"test_test_bdct4x4_hp_nn_decode_{num_freq_comp_outsourced}"
-best_model_path = f"/workspace/uwing2/Privatar/training_results/bdct_nn_decoder/best_model.pth"
+best_model_path = f"/workspace/uwing2/Privatar/training_results/bdct_hp_nn_decoder_{num_freq_comp_outsourced}/best_model.pth"
 result_path = f"{result_path_prefix}{project_name}"
 
 if not os.path.exists(result_path):
     os.makedirs(result_path)
-print(f'python test_sgl_run_uwing2.py --data_dir {data_dir}/dataset/m--20180227--0000--6795937--GHS --krt_dir {data_dir}/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_test {data_dir}/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path {result_path} --test_segment "./test_segment.json" --lambda_screen 1 --model_path {best_model_path}  --arch base  --project_name {project_name} --author_name jimmytong --val_batch_size {val_batch_size} --save_latent_code')
-os.system(f'python test_sgl_run_uwing2.py --data_dir {data_dir}/dataset/m--20180227--0000--6795937--GHS --krt_dir {data_dir}/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_test {data_dir}/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path {result_path} --test_segment "./test_segment.json" --lambda_screen 1 --model_path {best_model_path}   --arch base  --project_name {project_name} --author_name jimmytong --val_batch_size {val_batch_size} --save_latent_code')
+print(f'python test_sgl_run_uwing2.py --data_dir {data_dir}/dataset/m--20180227--0000--6795937--GHS --krt_dir {data_dir}/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_test {data_dir}/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path {result_path} --test_segment "./test_segment.json" --lambda_screen 1 --model_path {best_model_path}  --arch base  --project_name {project_name} --author_name jimmytong --val_batch_size {val_batch_size} --num_freq_comp_outsourced {num_freq_comp_outsourced}  --save_latent_code')
+os.system(f'python test_sgl_run_uwing2.py --data_dir {data_dir}/dataset/m--20180227--0000--6795937--GHS --krt_dir {data_dir}/dataset/m--20180227--0000--6795937--GHS/KRT --framelist_test {data_dir}/dataset/m--20180227--0000--6795937--GHS/frame_list.txt  --result_path {result_path} --test_segment "./test_segment.json" --lambda_screen 1 --model_path {best_model_path}   --arch base  --project_name {project_name} --author_name jimmytong --val_batch_size {val_batch_size} --num_freq_comp_outsourced {num_freq_comp_outsourced}  --save_latent_code')
