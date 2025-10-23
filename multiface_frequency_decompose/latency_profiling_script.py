5519fc5202dd469e27cc2933b3792e31518ea643
Jianming Tong <jianming.gatech@gmail.com>
2025-10-23 16:41:44 +0000

    update pure decomposition latency profile

--- multiface_frequency_decompose/latency_profiling_script.py
+++ multiface_frequency_decompose/latency_profiling_script.py
@@ -1,8 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-# 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
 
 from PIL import Image, ImageFile
 
@@ -15,30 +10,54 @@ import os
 import time
 from collections import OrderedDict
 
-import cv2
 import numpy as np
 from tqdm import tqdm
 import torch
 from models import DeepAppearanceVAE_Partition
 
-result_path = "/tmp/tmp"
-save_latent_code = False
-batch_size = 16
+use_traced_model = True
 
 device = torch.device("cuda", 0)
-
+batch_size = 1
 model = DeepAppearanceVAE_Partition(
-    1024, 21918, n_latent=256, n_cams=38, result_path=result_path, save_latent_code=save_latent_code
+    1024, 21918, n_latent=256, n_cams=38
 ).to(device)
+model.eval()
 
-z = torch.zeros(batch_size, 256).to("cuda:0")
-v = torch.zeros(batch_size, 3).to("cuda:0")
+z_local = torch.randn(batch_size, 256).to(device)
+v = torch.randn(batch_size, 3).to(device)
+
+# Create a traced model for faster inference
+if use_traced_model:
+  class TracedDecoder(torch.nn.Module):
+    def __init__(self, model_dec):
+        super().__init__()
+        self.model_dec = model_dec
+        
+    def forward(self, z_local, v):
+        return self.model_dec(z_local, v)
+      
+  traced_model = TracedDecoder(model.dec)
+  traced_model.eval()
+
+  # Trace the model
+  with torch.no_grad():
+    traced_model = torch.jit.trace(traced_model, (z_local, v))
+
+  # Trace the model
+  with torch.no_grad():
+     for _ in range(10):
+        _ = traced_model(z_local, v)
 
 start_time = time.time()
-
-for i in tqdm(range(1000)):
-    model.dec(z, v)
+total_inference = 10000
+if use_traced_model:
+  for i in tqdm(range(total_inference)):
+      traced_model(z_local, v)
+else:
+  for i in tqdm(range(total_inference)):
+      model.dec(z_local, v)
 
 end_time = time.time()
 
-print(f"Under Batchsize = {batch_size}, inference latency on GPU = {(end_time - start_time) / 2}")
+print(f"Under Batchsize = {batch_size}, inference latency on GPU = {(end_time - start_time) / total_inference}")
